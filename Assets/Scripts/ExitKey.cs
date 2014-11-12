7721f371fd095125021fe90881acd7d2dffc1dd6
Petar Petkov <petar.b.petkov@gmail.com>
2014-11-12 02:57:44 -0600

    initial commit

---
+++ Assets/Scripts/ExitKey.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExitKey : MonoBehaviour {
+
+	public void Update() {
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+        Application.Quit();
+    }
+}
+	}
